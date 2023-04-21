5fc9f1828ba5f0214256b4bd917cc3bca95ab779
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.8>
2023-04-21 10:00:18 -0300

    feat: like ccomments

--- 01-fundamentals/src/components/Comment.jsx
+++ 01-fundamentals/src/components/Comment.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+
 import styles from "./Comment.module.css";
 
-import { ThumbsUp, Trash } from "@phosphor-icons/react";
 import { Avatar } from "./Avatar";
 
 export function Comment({ content, onDeleteComment }) {
+	const [likeCount, setLikeCount] = useState(0);
 	function handleDeleteComment() {
 		onDeleteComment(content);
+	}
+
+	function handleLikeComment() {
+		setLikeCount((state) => state + 1);
 	}
 
 	return (
@@ -31,9 +38,9 @@ export function Comment({ content, onDeleteComment }) {
 				</div>
 
 				<footer>
-					<button>
+					<button onClick={handleLikeComment}>
 						<ThumbsUp />
-						Aplaudir <span>20</span>
+						Aplaudir <span>{likeCount}</span>
 					</button>
 				</footer>
 			</div>
