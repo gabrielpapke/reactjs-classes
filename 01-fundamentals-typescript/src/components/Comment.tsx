cd9e7e71a807b8da7f8d884e30a81327f8e092f6
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.8>
2023-04-22 07:56:40 -0300

    feat: initial project with typescript

---
+++ 01-fundamentals-typescript/src/components/Comment.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+
+import styles from "./Comment.module.css";
+
+import { Avatar } from "./Avatar";
+
+export function Comment({ content, onDeleteComment }) {
+	const [likeCount, setLikeCount] = useState(0);
+	function handleDeleteComment() {
+		onDeleteComment(content);
+	}
+
+	function handleLikeComment() {
+		setLikeCount((state) => state + 1);
+	}
+
+	return (
+		<div className={styles.comment}>
+			<Avatar hasBorder={false} src="https://github.com/gabrielpapke.png" />
+
+			<div className={styles.commentBox}>
+				<div className={styles.commentContent}>
+					<header>
+						<div className={styles.authorAndTime}>
+							<strong>Gabriel Papke</strong>
+							<time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">
+								Cerca de 1h atrás
+							</time>
+						</div>
+
+						<button title="Deletar comentário" onClick={handleDeleteComment}>
+							<Trash size={24} />
+						</button>
+					</header>
+
+					<p>{content}</p>
+				</div>
+
+				<footer>
+					<button onClick={handleLikeComment}>
+						<ThumbsUp />
+						Aplaudir <span>{likeCount}</span>
+					</button>
+				</footer>
+			</div>
+		</div>
+	);
+}
