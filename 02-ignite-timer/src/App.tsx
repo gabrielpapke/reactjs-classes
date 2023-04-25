68a6ede49506187d67a1612735b91d97f9cfd298
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.8>
2023-04-25 11:43:53 -0300

    feat: first styled components

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,3 +1,13 @@
+import { Button } from "./components/Button";
+
 export function App() {
-	return <>Hello world</>;
+	return (
+		<>
+			<Button variant="primary" />
+			<Button variant="secondary" />
+			<Button variant="success" />
+			<Button variant="danger" />
+			<Button />
+		</>
+	);
 }
