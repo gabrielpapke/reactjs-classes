6fc4396a2a26f5f6bf8187b32e10da227b277666
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.10>
2023-06-24 17:51:29 -0300

    feat: estrutura inicial

--- 03-dt-money/src/App.tsx
+++ 03-dt-money/src/App.tsx
@@ -1,7 +1,10 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       Hello world
-    </>
+    </ThemeProvider>
   )
 }
