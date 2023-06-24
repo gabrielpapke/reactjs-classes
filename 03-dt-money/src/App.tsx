5e13b54e5d10ab96eb30b79501cd12e29a86e51d
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.10>
2023-06-24 18:25:22 -0300

    feat: transactions page

--- 03-dt-money/src/App.tsx
+++ 03-dt-money/src/App.tsx
@@ -1,10 +1,15 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
+import { Transactions } from "./pages/Transactions";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      Hello world
+      <Transactions />
+
+      <GlobalStyle />
     </ThemeProvider>
+
   )
 }
