f43c840a36185c7dc414c266b7112ca9b45e11e7
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.10>
2023-06-28 08:09:13 -0300

    feat: fetching by params

--- 03-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
+++ 03-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,10 +1,12 @@
+import { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 
+import { TransactionsContext } from '../../../../contexts/TransactionsContext';
+
 import { MagnifyingGlass } from "@phosphor-icons/react";
 import { SearchFormContainer } from "./styles";
-
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -13,6 +15,8 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+  const { fetchTransactions } = useContext(TransactionsContext);
+
   const {
     register,
     handleSubmit,
@@ -22,9 +26,7 @@ export function SearchForm() {
   });
 
   async function handleSearchTransactions(data: SearchFormInputs) {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-
-    console.log(data);
+    await fetchTransactions(data.query)
   }
 
   return (
